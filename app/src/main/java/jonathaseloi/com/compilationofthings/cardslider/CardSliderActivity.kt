b18c3b47d30b677bd3a0c13909b0c0835ad1af98
Jonathas Eloi <jonathaseloi@gmail.com>
2019-09-18 10:09:55 -0300

    .

---
+++ app/src/main/java/jonathaseloi/com/compilationofthings/cardslider/CardSliderActivity.kt
@@ -0,0 +1,13 @@
+package jonathaseloi.com.compilationofthings.cardslider
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import jonathaseloi.com.compilationofthings.R
+
+class CardSliderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_slider)
+    }
+}
